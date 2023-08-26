e0e87989ff255821d2bf855a12f95ff07a017446
Milagros Luna <milivictoria2004@gmail.com>
2023-08-25 23:03:57 -0300

    fixing carrousel

--- toogleSidebar.js
+++ toogleSidebar.js
@@ -37,3 +37,58 @@ const logOut = () => {
   }
 };
 btnlogOut.addEventListener("click", logOut);
+
+
+const prevButton = document.querySelector('.btn-carousel-prev');
+const nextButton = document.querySelector('.btn-carousel-next');
+const carousel = document.getElementById('carousel');
+
+let currentIndex = 0;
+
+prevButton.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + 3) % 3; // Circular navigation
+  updateCarousel();
+});
+
+nextButton.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % 3; // Circular navigation
+  updateCarousel();
+});
+
+function updateCarousel() {
+  const images = carousel.querySelectorAll('img');
+  images.forEach((img, index) => {
+    if (index === currentIndex) {
+      img.classList.remove("hidden");
+    } else {
+      img.classList.add("hidden");
+    }
+  });
+}
+
+updateCarousel();
+
+
+
+// Function to be executed when the media query changes
+function handleMediaQueryChange(mq) {
+  if (!mq.matches) {
+    const images = carousel.querySelectorAll('img');
+    images.forEach((img, index) => {
+      
+      img.classList.remove("hidden");
+      
+    });
+  } else{
+    updateCarousel();
+  }
+}
+
+// Create a media query
+var mediaQuery = window.matchMedia("(max-width: 450px)");
+
+// Initial execution of the function
+handleMediaQueryChange(mediaQuery);
+
+// Add a resize event listener to track changes in the media query
+mediaQuery.addListener(handleMediaQueryChange);
