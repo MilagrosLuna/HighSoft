e611279e5624fa1e70f501395067d97aa452925f
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2023-09-07 19:55:56 -0300

    agregando algo de estilos a homepage

--- src/components/main-modules/home-page/HomePage.js
+++ src/components/main-modules/home-page/HomePage.js
@@ -7,12 +7,13 @@ const HomePage = () => {
 
   return (
     <div className="container mx-auto bg-secondary text-white text-center">
-      <div style={{width:40+'rem', height:15+'em'}} className="container mx-auto">
+      <div style={{width:40+'rem', height:15+'em', padding:2+'em'}} className="container mx-auto">
         <h1>Bienvenido a tu cuenta</h1>
-        <h3 className="text-white">Saldo disponible</h3>
-        <div className="center-block container">
+        <h3 style={{paddingTop:1+'em'}} className="text-white">Saldo disponible</h3>
+        <div className="center-block container" style={{paddingTop:0.6+'em'}}>
           {balance.showBalance ? "$"+balance.balance : "***"}
           <button
+            style={{marginLeft:3+'em'}}
             className="btn btn-default"
             id="saldoButton"
             onClick={balance.toggleShow}
