23ef0722a88a01a9a13db5a940019a99b7360ab2
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2023-09-08 19:51:09 -0300

    editando el movement history

--- src/components/main-modules/movements-history/MovementsHistory.js
+++ src/components/main-modules/movements-history/MovementsHistory.js
@@ -2,9 +2,16 @@ import React, { useContext, useEffect } from "react";
 import BalanceContext from "../../../context/BalanceContext";
 import MovementLog from "./MovementLog";
 import MovementLogContainer from "./MovementLogContainer";
+import { useState } from "react";
 
 const MovementsHistory = () => {
   const balance = useContext(BalanceContext);
+  
+  const [movementsIsRight, setMovementsIsRight] = useState(false)
+
+  // const checkMovementsArray = () => {
+  //   balance.movementsArray.length>1?setMovementsIsRight(true):null;
+  // } 
 
   useEffect(() => {
     console.log(balance.movementsArray);
@@ -14,7 +21,7 @@ const MovementsHistory = () => {
     <div  style={{width:60+"%"}}  className="container py-3 mx-auto my-3 text-white text-center bg-rosa rounded">
       <h3 className="text-white">Este es el historial de tus movimientos</h3>
       <MovementLogContainer>
-      {balance.movementsArray.length != 0
+      {balance.movementsArray.length != 1
         ? balance.movementsArray.map((element) => (
             <MovementLog
               date={element.date}
--- src/hooks/useMovement.js
+++ src/hooks/useMovement.js
@@ -39,15 +39,17 @@ const useMovement = () => {
   // se crea un objeto con todos los datos recolectados y se envian a el array 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if(balance.decrement(amount)){
-      const newMovement = {
-        type: typeOfMovement,
-        date: date,
-        amount: amount,
-        beneficiary: beneficiary,
-      };
-      balance.setMovementsArray([newMovement, ...balance.movementsArray]);
-      console.log(balance.movementsArray);
+    if(amount > 0){
+      if(balance.decrement(amount)){
+        const newMovement = {
+          type: typeOfMovement,
+          date: date,
+          amount: amount,
+          beneficiary: beneficiary,
+        };
+        balance.setMovementsArray([newMovement, ...balance.movementsArray]);
+        console.log(balance.movementsArray);
+      }
     }
   };
 
