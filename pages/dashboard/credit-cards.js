e8f25de80913495f44479313bc6544070daccf84
agustinbarrionuev <agustinbarrionuev@gmail.com>
2023-09-25 10:54:24 -0300

    adding credit-cards.js and credit-cards.css

---
+++ pages/dashboard/credit-cards.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import './credit-cards.css';
+
+
+// Componente de tarjeta de crédito
+function CreditCard({ type, limit, showDetails }) {
+  return (
+    <div className="card">
+      <h2>{type} Card</h2>
+      <p>Limit: ${limit}</p>
+      {showDetails && (
+        <div className="card-details">
+          {/* Agregar detalles adicionales aquí */}
+        </div>
+      )}
+      <button className="btn" onClick={showDetails}>
+        {showDetails ? 'Hide Details' : 'Show Details'}
+      </button>
+      <button className="btn">Request Limit Increase</button>
+      <button className="btn">View Statements</button>
+    </div>
+  );
+}
+
+function CreditCards() {
+  const [showCardDetails, setShowCardDetails] = useState(false);
+
+  return (
+    <div className="credit-cards">
+      <h1>Your Credit Cards</h1>
+      <CreditCard type="Credit" limit={5000} showDetails={() => setShowCardDetails(!showCardDetails)} />
+      <CreditCard type="Credit" limit={8000} showDetails={() => setShowCardDetails(!showCardDetails)} />
+      <CreditCard type="Debit" limit={2000} showDetails={() => setShowCardDetails(!showCardDetails)} />
+    </div>
+  );
+}
+
+export default CreditCards;
