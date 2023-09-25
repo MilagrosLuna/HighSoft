d8cd2733980208bbd6a6bd86b56e412c94068d04
agustinbarrionuev <agustinbarrionuev@gmail.com>
2023-09-25 13:08:18 -0300

    Update navHome.js

--- components/navHome.js
+++ components/navHome.js
@@ -100,7 +100,16 @@ function HomeNav() {
                 </li>
                 <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
                   <Link
-                    href="/"
+                    href="/dashboard/transfers"
+                    onClick={() => setNavbar(!navbar)}
+                   
+                  >
+                    Tarjetas
+                  </Link>
+                </li>
+                <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                  <Link
+                    href="/dashboard/credit-cards"
                     onClick={() => setNavbar(!navbar)}
                     style={{                     
                       color: "white",                     
