2067b2bd43f68cd78ca8f8ba70f7c7db57f9e89b
MatiasC00 <141969042+MatiasC00@users.noreply.github.com>
2023-10-23 20:14:32 -0300

    Update clientes.py
    documentacion

--- clientes/clientes.py
+++ clientes/clientes.py
@@ -1,8 +1,42 @@
 class Cliente:
+     """
+    Clase que representa a un cliente del banco.
+
+    Atributos:
+        - numero (int): Número de cliente.
+        - nombre (str): Nombre del cliente.
+        - apellido (str): Apellido del cliente.
+        - dni (int): Número de documento del cliente.
+        - transacciones (list): Lista de transacciones realizadas por el cliente.
+
+    Métodos:
+        - __init__(numero, nombre, apellido, dni): Constructor de la clase que inicializa un cliente.
+        - agregar_transaccion(transaccion): Agrega una transacción a la lista de transacciones del cliente.
+        - calcular_monto_total(precio_dolar, monto): Calcula el monto total considerando el precio del dólar, el impuesto país y las ganancias.
+        - descontar_comision(monto, comision_porcentaje): Descuenta una comisión del monto especificado.
+        - calcular_monto_plazo_fijo(monto_inicial, interes_porcentaje): Calcula el monto final de una inversión a plazo fijo.
+
+    Atributos de Clase:
+        - impuesto_pais (float): Tasa del impuesto país.
+        - ganancias (float): Tasa de ganancias.
+
+    Excepciones:
+        - ExcepcionLimite: Excepción personalizada que se lanza cuando se alcanza un límite.
+
+    """
     impuesto_pais = 0.30 
     ganancias = 0.35
 
     def __init__(self,numero,nombre,apellido,dni) -> None:
+          """
+        Inicializa un cliente.
+
+        Args:
+            - numero (int): Número de cliente.
+            - nombre (str): Nombre del cliente.
+            - apellido (str): Apellido del cliente.
+            - dni (int): Número de documento del cliente.
+        """
 
         if not isinstance(numero,int) or numero<0:
                 raise ValueError("El numero de cliente tiene que ser INT y no puede ser negativo")
@@ -20,10 +54,26 @@ class Cliente:
         self.transacciones = [] 
 
     def agregar_transaccion(self, transaccion):
+        """
+        Agrega una transacción a la lista de transacciones del cliente.
+
+        Args:
+            - transaccion: Transacción a agregar.
+        """
         self.transacciones.append(transaccion)
 
 
     def calcular_monto_total(self, precio_dolar, monto):
+        """
+        Calcula el monto total considerando el precio del dólar, el impuesto país y las ganancias.
+
+        Args:
+            - precio_dolar (int o float): Precio actual del dólar.
+            - monto (int o float): Monto a adquirir.
+
+        Returns:
+            - float: Monto total calculado.
+        """
         
         if not (isinstance(precio_dolar, (int, float)) and precio_dolar > 0):
             raise ValueError("El precio del dólar debe ser un número positivo.")
@@ -34,11 +84,23 @@ class Cliente:
         impuesto = monto_total * self.impuesto_pais
         ganancia = monto_total * self.ganancias
         monto_total += impuesto + ganancia
+        
+        # Implementación del cálculo del monto total
 
         return monto_total
     
 
     def descontar_comision(self, monto, comision_porcentaje):
+        """
+        Descuenta una comisión del monto especificado.
+
+        Args:
+            - monto (int o float): Monto original.
+            - comision_porcentaje (int o float): Comisión expresada en porcentaje.
+
+        Returns:
+            - float: Monto después de aplicar el descuento de la comisión.
+        """
         
         if not (isinstance(monto, (int, float)) and monto >= 0):
             raise ValueError("El monto debe ser un número no negativo.")
@@ -48,8 +110,20 @@ class Cliente:
         comision = (comision_porcentaje / 100) * monto
         monto_descontado = monto - comision
         return monto_descontado
+
+        # Implementación del descuento de comisión
     
     def calcular_monto_plazo_fijo(self, monto_inicial, interes_porcentaje):
+        """
+        Calcula el monto final de una inversión a plazo fijo.
+
+        Args:
+            - monto_inicial (int o float): Monto inicial de la inversión.
+            - interes_porcentaje (int o float): Tasa de interés expresada en porcentaje.
+
+        Returns:
+            - float: Monto final calculado.
+        """
 
         if not (isinstance(monto_inicial, (int, float)) and monto_inicial >= 0):
             raise ValueError("El monto inicial debe ser un número no negativo.")
@@ -58,6 +132,8 @@ class Cliente:
 
         monto_final = monto_inicial + (monto_inicial * (interes_porcentaje / 100))
         return monto_final
+
+        # Implementación del cálculo del monto final
     
 
 class ExpcionLimite(Exception):
