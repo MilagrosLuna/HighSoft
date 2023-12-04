3032aed0361431f38b28c3d9696def75cf5b9717
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2023-12-04 20:51:42 -0300

    modifying the accounts endpoint to display only the accounts of the logged-in client

--- homebanking/homebanking/Cuentas/views.py
+++ homebanking/homebanking/Cuentas/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from Cuentas.models import Cuenta, TipoCuenta
 from Cuentas.utils import get_iban, get_tipo_cuenta
+from Clientes.models import Cliente
 from common.utils import format_number, get_branch_id
 
 # Imports for ViewSets
@@ -17,11 +18,23 @@ from rest_framework.authentication import BasicAuthentication
 
 # contra 684WY2mHfICk1BXLHerh3
 
+
 class CuentaViewset(viewsets.ModelViewSet):
-    
+
     queryset = Cuenta.objects.all()
     serializer_class = CuentaSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def list(self, request):
+        user_id = request.user.id
+        client = Cliente.objects.filter(user_id=user_id)
+        client_id = client[0].customer_id
+
+        queryset = Cuenta.objects.filter(client_id=client_id)
+        serializer = CuentaSerializer(queryset, many=True)
+
+        return response.Response(serializer.data, status=status.HTTP_200_OK)
+
 
 class CrearCuentaViewSet(viewsets.ModelViewSet):
 
@@ -30,33 +43,28 @@ class CrearCuentaViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
     def create(self, request):
-         # Acceder a los datos de la solicitud POST
         data = request.data
 
-        # Crear una nueva instancia de TuModelo usando el serializador
         serializer = self.get_serializer(data=data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
-
-        # Devolver una respuesta indicando que el objeto se creó con éxito
         headers = self.get_success_headers(serializer.data)
-        return response.Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-    
-class CrearUserViewSet(viewsets.ModelViewSet):
 
+        return response.Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class CrearUserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = CrearUserSerializer
     permission_classes = [permissions.AllowAny]
 
     def create(self, request):
-         # Acceder a los datos de la solicitud POST
+
         data = request.data
 
-        # Crear una nueva instancia de TuModelo usando el serializador
-        serializer = self.get_serializer(data  =data)
-        serializer.is_valid(raise_exception = True)
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
 
-        # Devolver una respuesta indicando que el objeto se creó con éxito
         headers = self.get_success_headers(serializer.data)
         return response.Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
