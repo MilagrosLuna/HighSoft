b188955cf2ab803d4ae3092d4932e22c311a7951
Gonzalo Jaime Guiñazu <131501955+GzaJai@users.noreply.github.com>
2023-12-10 22:05:18 +0000

    changing the url in the utils/api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const urlPrueba = "https://stunning-potato-qw4v5qwwr9xf695v-8000.app.github.dev"
 
 const API = axios.create({
-  // baseURL: "http://127.0.0.1:8000",
-  baseURL: urlPrueba,
+  baseURL: "http://127.0.0.1:8000",
+  // baseURL: urlPrueba,
 });
 
 export default API
